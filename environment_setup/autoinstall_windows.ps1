a32ae70433ee71e2d392a890c0b343d989e1e356
Simeon Wong <7825879+dtxe@users.noreply.github.com>
2024-11-18 12:47:19 -0500

    Update autoinstall_windows.ps1

--- environment_setup/autoinstall_windows.ps1
+++ environment_setup/autoinstall_windows.ps1
@@ -62,16 +62,7 @@ else {
 # update path
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User") 
 
-# add Git Bash to Windows Terminal using JSON Fragments
-$git_dir = Split-Path -parent (Split-Path -parent (Get-Command git.exe).Source)
-$terminal_settings = @{guid = "{" + [guid]::NewGuid().ToString() + "}"; name = "Git Bash"; commandline = $git_dir + "\bin\bash.exe -i -l"; icon = $git_dir + "\mingw64\share\git\git-for-windows.ico"; hidden = $false; startingDirectory = "%USERPROFILE%" }
-$terminal_settings = @{ profiles = @($terminal_settings) }
-$terminal_settings = $terminal_settings | ConvertTo-Json
-if (!(Test-Path "C:\Users\$env:UserName\AppData\Local\Microsoft\Windows Terminal\Fragments\Git")) {
-    mkdir "C:\Users\$env:UserName\AppData\Local\Microsoft\Windows Terminal\Fragments\Git"
-}
-$terminal_settings | Out-File -FilePath "C:\Users\$env:UserName\AppData\Local\Microsoft\Windows Terminal\Fragments\Git\gitbash.json" -Encoding Utf8
-
+# NOTE: Git Bash is now auto detected by Windows Terminal and manual installation is no longer required (and would be redundant)
 
 ###################################################################################
 # check for vscode
